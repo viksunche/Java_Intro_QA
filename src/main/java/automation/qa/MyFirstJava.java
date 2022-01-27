0b4d4716923e158044f4a5735afa5fbc5590d057
viki <vici.atanasova@gmail.com>
2022-01-27 22:48:15 +0200

    New consoles added

--- src/main/java/automation/qa/MyFirstJava.java
+++ src/main/java/automation/qa/MyFirstJava.java
@@ -1,4 +1,31 @@
 package automation.qa;
 
+import java.util.Scanner;
+
 public class MyFirstJava {
+
+    public static void main(String[] args) {
+
+        //write the text in the console
+        System.out.println("  Please enter your name: " );
+
+        //Scanner is class for reading values in console
+        Scanner scanner = new Scanner(System.in);
+
+        String customer = scanner.next();
+
+        System.out.println(" Read the name of customer: " +
+                customer);
+
+        System.out.println( " Please enter your hobby: ");
+
+        Scanner scanner1 = new Scanner(System.in);
+
+        String hobby = scanner.next();
+
+        System.out.println( " Read the hobby of customer: " + hobby);
+
+    }
+
+
 }
